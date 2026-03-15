bc70c95b4e09b8ef1fb29dfbda5990778a4f1f3e
S1ahs3r <Slahser@users.noreply.github.com>
2026-03-15 23:39:09 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -70,8 +70,7 @@ const workerConfig: WorkerConfig = {
   notification: {
     // [Optional] Notification webhook settings, if not specified, no notification will be sent
     // More info at Wiki: https://github.com/lyc8503/UptimeFlare/wiki/Setup-notification
-    webhook: {
-    },
+    webhook: null,
     // [Optional] timezone used in notification messages, default to "Etc/GMT"
     timeZone: 'Asia/Shanghai',
     // [Optional] grace period in minutes before sending a notification
